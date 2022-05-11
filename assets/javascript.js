90f2fb12772031011f1f2abeda4b75de6b1ef8fc
Kmac5000 <102488109+Kmac5000@users.noreply.github.com>
2022-05-10 20:02:41 -0600

    questions cycle, answers work

--- assets/javascript.js
+++ assets/javascript.js
@@ -6,33 +6,33 @@ var askQuestion = ".quesHead";
 var questions = [ // array of objects
     {
         // question 0
-        question: "Commonly used data types do NOT include:",
-        answers: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
+        question: "Wednesday's favorite toy is a:",
+        answers: ["stick", "frog", "ball", "carpet square"],
         correctAnswer: "3"
     },
     {
         // question 1
-        question: "The condition in an if / else statement is enclosed within ____.",
-        answers: ["1. quotes", "2. curly brackets", "3. parentheses", "4. square brackets"],
+        question: "What color is Lorelia?",
+        answers: ["black", "yellow", "brown", "pink"],
         correctAnswer: "3"
     },
     {
         // question 2
-        question: "Arrays in Javascript can be used to store ____.",
-        answers: ["1. numbers and strings", "2. other arrays", "3. booleans", "4. all of the above"],
+        question: "Sam is...",
+        answers: ["gigantic", "fluffy", "made of wood", "bada** mf"],
         correctAnswer: "4"
     },
     {
         // question 3
-        question: "String values must be enclosed within ____ when being assigned to variables.",
-        answers: ["1. commmas", "2. curly brackets", "3. quotes", "4. parentheses"],
-        correctAnswer: "3"
+        question: "Cletus was..",
+        answers: ["super cuddly", "really big", "my best bud", "all of the above"],
+        correctAnswer: "4"
     },
     {
         // question 4
-        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
-        answers: ["1. Javascript", "2. terminal/bash", "3. for loops", "4. console.log"],
-        correctAnswer: "4"
+        question: "Lorelia is hungry when?",
+        answers: ["morning", "afternoon", "both a & b", "none of the above"],
+        correctAnswer: "3"
     }
 ];
 
@@ -71,10 +71,10 @@ $(".ansBox").on("click",
 
 
         
-        if (questions.correctAnswer === event.target.value) {
-            alert("Correct!");
-        } else if (questions.correctAnswer !== event.target.value) {
-            alert("Wrong!");
+        if (questions[questionCount].correctAnswer === event.target.value) {
+            alert("Correct! "  + questions[questionCount].correctAnswer);
+        } else if (questions[questionCount].correctAnswer !== event.target.value) {
+            alert("Wrong! " +  questions[questionCount].correctAnswer);
         }
 
         if (questionCount < questions.length) {
